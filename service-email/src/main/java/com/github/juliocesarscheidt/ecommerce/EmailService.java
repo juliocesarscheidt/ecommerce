a4b357fc71c3690321c3c1785c384b66ce2c814a
juliocesarscheidt <julio.scheidt96@gmail.com>
2022-10-12 00:56:50 -0300

    Change email service

--- service-email/src/main/java/com/github/juliocesarscheidt/ecommerce/EmailService.java
+++ service-email/src/main/java/com/github/juliocesarscheidt/ecommerce/EmailService.java
@@ -1,8 +1,55 @@
 package com.github.juliocesarscheidt.ecommerce;
 
+import java.util.Properties;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import jakarta.mail.Address;
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
 public class EmailService {
+
+	private void sendEmail(String emailDestination, String emailBody) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "172.16.0.3");
+	    // props.put("mail.smtp.socketFactory.port", "465");
+	    props.put("mail.smtp.socketFactory.port", "1025");
+	    // props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+	    props.put("mail.smtp.auth", "false");
+	    // props.put("mail.smtp.port", "465");
+	    props.put("mail.smtp.port", "1025");
+
+	    Session session = Session.getDefaultInstance(props, new Authenticator() {
+	    	protected PasswordAuthentication getPasswordAuthentication() {
+        	   return new PasswordAuthentication("", "");
+	    	}
+	    });
+	    session.setDebug(true);
+
+	    try {
+	        Message message = new MimeMessage(session);
+	        message.setFrom(new InternetAddress("sender@mailhog.com"));
+	        Address[] destinationUser = InternetAddress.parse(emailDestination);
+
+	        message.setRecipients(Message.RecipientType.TO, destinationUser);
+	        message.setSubject("Ecommerce welcome");
+	        message.setText(emailBody);
+
+	        Transport.send(message);
+
+	        System.out.println("Email sent");
+	
+	     } catch (MessagingException e) {
+	    	 throw new RuntimeException(e);
+	     }
+	}
 
 	private void parse(ConsumerRecord<String, Email> record) {
 		System.out.println("[INFO] key " + record.key()
@@ -12,10 +59,10 @@ public class EmailService {
 						  + " | offset " + record.offset());
 		
 		Email emailContent = record.value();
-		String email = emailContent.getSubject();
+		String emailDestination = emailContent.getSubject();
 		String emailBody = emailContent.getBody();
-
-		System.out.println("Pretending we would send an email to the user");
+		
+		sendEmail(emailDestination, emailBody);
 	}
 
 	public static void main(String[] args) {
